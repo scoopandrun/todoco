7950eae700c03eab23174d3521fad8c2e710997b
Nicolas DENIS <me@scoopandrun.ovh>
2024-06-02 13:50:16 +0200

    Update tests

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,6 +3,7 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -10,12 +11,33 @@ class DefaultControllerTest extends WebTestCase
     {
         // Given
         $client = static::createClient();
+        $method = 'GET';
+        $url = '/';
 
         // When
-        $client->request('GET', '/');
+        $client->request($method, $url);
+        $response = $client->getResponse();
 
         // Then
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect('http://localhost/login'));
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        /** @var RedirectResponse $response */
+        $this->assertEquals('http://localhost/login', $response->getTargetUrl());
+    }
+
+    public function testAuthenticatedIndexReturns200()
+    {
+        // Given
+        $client = static::createClient([], [
+            'PHP_AUTH_USER' => 'User1',
+            'PHP_AUTH_PW' => 'pass123',
+        ]);
+        $method = 'GET';
+        $url = '/';
+
+        // When
+        $client->request($method, $url);
+
+        // Then
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
 }
