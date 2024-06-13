f72cb37818daa58a0444e5b78a39d58bf98352bc
Nicolas DENIS <me@scoopandrun.ovh>
2024-06-13 21:54:10 +0200

    Fix "risky test" warning after upgrading to PHPUnit 11

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
+use Symfony\Component\ErrorHandler\ErrorHandler;
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// https://github.com/symfony/symfony/issues/53812#issuecomment-1962740145
+set_exception_handler([new ErrorHandler(), 'handleException']);
 
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
