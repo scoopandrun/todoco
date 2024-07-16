d4aa9d93cf367f368191a27d931fa4ee90184193
Nicolas DENIS <me@scoopandrun.ovh>
2024-07-16 17:42:40 +0200

    Fix exception output

--- tests/Controller/ClientTrait.php
+++ tests/Controller/ClientTrait.php
@@ -31,7 +31,7 @@ trait ClientTrait
         $user = $this->getUser($username);
 
         if (null === $user) {
-            throw new \RuntimeException("User \"{$username}\" not found.");
+            throw new \RuntimeException(sprintf('User "%s" not found.', htmlentities($username)));
         }
 
         $client->loginUser($user);
